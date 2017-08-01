3b84438418094eab4c6d80350f6c160d7075c241
Peter Arentsen <peter.arentsen@holder.nl>
2017-08-01 17:15:37 +0200

    Update mix with info needed for hex package

--- mix.exs
+++ mix.exs
@@ -5,9 +5,12 @@ defmodule ImageOptimizerEx.Mixfile do
     [app: :image_optimizer_ex,
      version: "0.1.0",
      elixir: "~> 1.3",
+     name: "Image Optimizer",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     description: description(),
+     package: package(),
+     deps: []]
   end
 
   # Configuration for the OTP application
@@ -17,16 +20,18 @@ defmodule ImageOptimizerEx.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    []
+  defp description do
+    """
+    Tool to optimize jpg/png images for web
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "README*", "mix.exs"],
+      maintainers: ["Peter Arentsen", "Vladimir Kravchenko"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/bettyblocks/image-optimizer-ex"}
+    ]
   end
 end
